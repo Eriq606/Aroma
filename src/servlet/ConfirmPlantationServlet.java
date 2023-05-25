3f2a556b1de1e9b2c67971debe56fac2862cba03
Eriq606 <erohweltall@gmail.com>
2023-05-25 03:21:00 +0300

    commit: insertion d'utilisation de ressource base

--- src/servlet/ConfirmPlantationServlet.java
+++ src/servlet/ConfirmPlantationServlet.java
@@ -7,11 +7,26 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import assets.Plantation;
+import assets.ProduitIntermediaire;
+
 public class ConfirmPlantationServlet extends HttpServlet{
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String idproduit=req.getParameter("produit");
         String quantite=req.getParameter("quantite");
         String date=req.getParameter("date");
+
+        ProduitIntermediaire produit=new ProduitIntermediaire();
+        produit.setId(idproduit);
+        Plantation plantation=new Plantation();
+        try{
+            produit.getProduitIntermediaireById(null);
+            plantation.setProduit(produit);
+            plantation.setQuantite(quantite);
+            plantation.setDate(date);
+        }catch(Exception e){
+
+        }
     }
 }
--- src/utils/Constantes.java
+++ src/utils/Constantes.java
@@ -5,7 +5,7 @@ public class Constantes {
     public static final String database="aroma";
     public static final String password="root";
 
-    public static final String col_id_produit="id";
+    public static final String col_id_produit="idproduit";
     public static final String col_nom_produit="nom";
 
     public static final String col_id_type="idtype";
@@ -18,9 +18,18 @@ public class Constantes {
     public static final String col_description_fournisseur="description";
     public static final String col_contact_fournisseur="contact";
 
+    public static final String col_id_produit_intermediaire="idplante";
+    public static final String col_id_ressource="idressource";
+
+    public static final String col_quantite_recette="quantite";
+    public static final String col_quantite_restant="quantite_restant"; 
+
     public static final String view_ressource="v_liste_ressource";
     public static final String view_produitIntermediaire="v_liste_produit_intermediaire";
 
+    public static final String view_stock="v_liste_stock";
+
     public static final String table_mouvement_produit="mouvementproduit";
     public static final String table_fournisseur="fournisseur";
+    public static final String table_recette="ressourceplante";
 }
